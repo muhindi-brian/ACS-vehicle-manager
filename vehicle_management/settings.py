3941e3d3596e3370de0c44d6ecca0c5749d22125
muhindi-brian <brian@brianecodes.africa>
2024-11-21 00:18:44 +0300

    "images"

--- vehicle_management/settings.py
+++ vehicle_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+ddw^+%tt4g112!)=5g^0=uxk5t%p6@cfx)^&$v-ht%ll%g9=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = '/dashboard/' 
 
